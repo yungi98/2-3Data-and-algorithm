cba2d8afbad4cd9192f55a02cd431f1cf70eb464
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:39:26 +0900

    자료구조와 게임 알고리즘 재귀함수 실습2
    n값을 입력 받아 1부터 n까지의 덧셈 구현하기

---
+++ Data/c++_10/c++_10/c++_10.cpp
@@ -0,0 +1,18 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int a)
+{
+    if (a == 1)
+        return 1;
+    else
+        return (sum(a - 1) + a);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sum(n);
+}
