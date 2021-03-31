f66827d37d780c88e7eb22037930e9b5a3772130
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:23:07 +0900

    자료구조와 게임 알고리즘 다차원 배열 실습
    다차원 배열을 이용하여 문자열을 3개 이상 입력 받아 입력 받은 문자열을 거꾸로 출력하는 프로그램을 작성하기

---
+++ Data/c++_3/c++_3/c++_3.cpp
@@ -0,0 +1,13 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char name[1][4] = {  };
+    cout << "3개의 문자열을 입력하세요 : ";
+    cin >> name[0];
+
+    cout << name[0][2] << name[0][1] << name[0][0] << endl;
+}
