30ab4dbd5844fcff982fd56cf4b50f9327f90ae9
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:25:43 +0900

    자료구조와 게임 알고리즘 재귀함수 실습1
    팩토리얼 함수 구현하기

---
+++ Data/c++_4/c++_4/c++_4.cpp
@@ -0,0 +1,18 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int Factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return(n * Factorial(n - 1));
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "1부터" << n << "까지의 곱은 " << Factorial(n) << "입니다." << endl;
+}
