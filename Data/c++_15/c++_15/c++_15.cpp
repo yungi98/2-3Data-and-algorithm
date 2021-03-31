b7cf39914918cb3b5d24888accbbcc72027289a9
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:46:48 +0900

    자료구조와 게임 알고리즘 재귀함수 실습4
    숫자 n을 입력받아 n부터 1까지의 숫자를 역순으로 출력하기

---
+++ Data/c++_15/c++_15/c++_15.cpp
@@ -0,0 +1,23 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int a)
+{
+	if (a == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		cout << a << endl;
+		return sum(a - 1);
+	}
+}
+
+int main()
+{
+	int a;
+	cin >> a;
+	sum(a);
+}
