731b85857dc8bf8bd844c9bde1e60b6e042bc97f
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:41:32 +0900

    자료구조와 게임 알고리즘 재귀함수 실습2
    입력받은 십진수를 거꾸로 출력하는 프로그램을 작성하기

---
+++ Data/c++_11/c++_11/c++_11.cpp
@@ -0,0 +1,39 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    if (a == b)
+    {
+        return b;
+    }
+    else if (a < b)
+    {
+        return sum(a + 1, b);
+    }
+    else
+    {
+        return sum(a - 1, b);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 10)
+    {
+        cout << n;
+    }
+    else if (n < 100)
+    {
+        int d = (n / 10) + ((n % 10) * 10);
+        cout << sum(n, d);
+    }
+    else if (n < 1000)
+    {
+        int d = (n / 100) + ((n % 100) - (n % 10)) + ((n % 10) * 100);
+        cout << sum(n, d);
+    }
+}
