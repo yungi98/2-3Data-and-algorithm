70631ee94d8c0714c62030d3507d55be9364b499
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:45:27 +0900

    자료구조와 게임 알고리즘 재귀함수 실습3
    구구단을 출력하는 프로그램 작성하기

---
+++ Data/c++_14/c++_14/c++_14.cpp
@@ -0,0 +1,25 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int a)
+{
+	if (a == 10)
+	{
+		return 0;
+	}
+	else
+	{
+		for (int i = 1; i < 10; i++)
+		{
+			cout << a << " x " << i << " = " << a * i << endl;
+		}
+		return sum(a + 1);
+	}
+}
+
+int main()
+{
+	int a = 1;
+	sum(a);
+}
