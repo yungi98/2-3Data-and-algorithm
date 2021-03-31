1ddf2065db6aa64eeaec2e48e5e808929591f2ce
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:43:37 +0900

    자료구조와 게임 알고리즘 재귀함수 실습3
    숫자를 5개 이상 입력 받아 그 중 최댓값을 출력하는 프로그램 작성하기

---
+++ Data/c++_12/c++_12/c++_12.cpp
@@ -0,0 +1,32 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int a, int i)
+{
+	cin >> a;
+	if (a == 0)
+	{
+		return (i);
+	}
+	else
+	{
+		if (i < a)
+		{
+			i = a;
+			return sum(a, i);
+		}
+		else
+		{
+			return sum(a, i);
+		}
+	}
+}
+
+int main()
+{
+	int a = 1;
+	int i = 1;
+	cout << "숫자 입력이 완료가 되면 0을 입력하세요" << endl;
+	cout << sum(a, i);
+}
