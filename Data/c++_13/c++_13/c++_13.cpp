6a2cb3b166647d39b12de143b922a82fa41a5976
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:44:36 +0900

    자료구조와 게임 알고리즘 재귀함수 실습3
    4자리 이상의 수를 입력 받아 각 자리수의 합을 구하는 프로그램 작성하기

---
+++ Data/c++_13/c++_13/c++_13.cpp
@@ -0,0 +1,25 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int i, int s)
+{
+	if (i / 10 == 0)
+	{
+		return s += (i % 10);
+	}
+	else
+	{
+		return sum(i = i / 10, s += (i % 10));
+	}
+}
+
+int main()
+{
+	int a;
+	int b = 0;
+	cout << "4자리 이상의 수를 입력해 주세요" << endl;
+	cin >> a;
+	cout << sum(a, b);
+
+}
