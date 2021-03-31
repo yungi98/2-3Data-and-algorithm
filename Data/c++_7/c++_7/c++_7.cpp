f463839201bbea8cf56f97d8fe5875f83939c964
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:33:15 +0900

    자료구조와 게임 알고리즘 재귀함수 실습1
    팩토리얼 함수를 재귀함수를 사용하지 않고 구현하기

---
+++ Data/c++_7/c++_7/c++_7.cpp
@@ -0,0 +1,18 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int Factorial(int n)
+{
+    int m = 1;
+    for (int i = 1; i <= n; ++i)
+        m *= i;
+    return m;
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << "1부터" << a << "까지의 곱은 " << Factorial(a) << "입니다." << endl;
+}
