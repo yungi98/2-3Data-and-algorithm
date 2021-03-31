7eb6753a7c3e6a0e5e992943820e1fa69de8449d
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:30:46 +0900

    자료구조와 게임 알고리즘 재귀함수 실습1
    가우스의 계산법을 사용하지 않고 1부터 100까지 덧셈 구현하기

---
+++ Data/c++_6/c++_6/c++_6.cpp
@@ -0,0 +1,23 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int gusno(int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return b + gusno(b - 1);
+    }
+
+}
+int main()
+{
+    int bb;
+    cin >> bb;
+    cout << gusno(bb) << endl;
+    return 0;
+}
