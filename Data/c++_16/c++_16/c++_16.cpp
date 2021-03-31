fdde263c4eff98d0c1bfe14e61be1bbd2c0a2a87
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:47:34 +0900

    자료구조와 게임 알고리즘 재귀함수 실습4
    숫자 2개를 입력 받아 두 수 사이의 홀수 출력하기

---
+++ Data/c++_16/c++_16/c++_16.cpp
@@ -0,0 +1,66 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+	if (a < b) // a = 1 b = 10
+	{
+		if (a == b) // a와 b가 같으면 탈출
+		{
+			return 0;
+		}
+		else // 아닐경우 반복
+		{
+			if (a % 2 == 0) // 짝수일 경우
+			{
+				return sum(a + 1, b);
+			}
+			else // 홀수일 경우
+			{
+				cout << a << endl;
+				return sum(a + 1, b);
+			}
+		}
+	}
+	else if (a > b) // a = 10 b = 1
+	{
+		if (a == b) // a와 b가 같으면 탈출
+		{
+			return 0;
+		}
+		else // 아닐경우 반복
+		{
+			if (b % 2 == 0) // 짝수일 경우
+			{
+				return sum(a, b + 1);
+			}
+			else // 홀수일 경우
+			{
+				cout << b << endl;
+				return sum(a, b + 1);
+			}
+		}
+	}
+	else // 숫자 2개가 같을 경우
+	{
+		if (a % 2 == 0) // 짝수일 경우
+		{
+			return 0;
+		}
+		else // 홀수일 경우
+		{
+			cout << a << endl;
+			return 0;
+		}
+	}
+}
+
+int main()
+{
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << endl;
+	sum(a, b);
+}
