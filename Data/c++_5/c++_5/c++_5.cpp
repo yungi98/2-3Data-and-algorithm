50d973465d81bf7accfb87731071ad9d93e35d29
최윤기 <dbsrldufao12@gmail.com>
2021-04-01 00:27:58 +0900

    자료구조와 게임 알고리즘 재귀함수 실습1
    가우스의 계산법 구현하기

---
+++ Data/c++_5/c++_5/c++_5.cpp
@@ -0,0 +1,16 @@
+﻿//20318 최윤기
+#include <iostream>
+using namespace std;
+
+int gus(int a)
+{
+    return (((1 + a) * a) / 2);
+}
+
+int main()
+{
+    int aa;
+    cout << "1부터 n까지의 수를 더합니다." << endl;
+    cin >> aa;
+    cout << "합은 : " << gus(aa);
+}
