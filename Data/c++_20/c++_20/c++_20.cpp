6e9765308df9c01041de19409a95e2d600350552
최윤기 <dbsrldufao12@gmail.com>
2021-04-19 16:52:57 +0900

    자료구조와 게임 알고리즘 스택실습1
    세 정수를 거꾸로 출력하기(스택 사용)

---
+++ Data/c++_20/c++_20/c++_20.cpp
@@ -0,0 +1,25 @@
+﻿//20318 최윤기
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> s;
+
+int main()
+{
+	int a = 0;
+
+	cout << "세개의 정수를 입력해주세요." << endl;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		cin >> a;
+		s.push(a);
+	}
+
+	for (int i = 1; i <= 3; i++)
+	{
+		cout << s.top() << " ";
+		s.pop();
+	}
+}
