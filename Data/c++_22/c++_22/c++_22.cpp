d0b29ded0279d1933ed8a02707e88352fb9978c1
최윤기 <dbsrldufao12@gmail.com>
2021-04-28 00:24:23 +0900

    자료구조와 게임 알고리즘 스택실습3
    거꾸로 출력하기(스택 사용)

---
+++ Data/c++_22/c++_22/c++_22.cpp
@@ -0,0 +1,25 @@
+﻿//20318 최윤기
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<string> s;
+
+int main()
+{
+	int a;
+	string b;
+	cout << "입력할 데이터의 개수를 입력해 주세요(n <= 1000)" << endl;
+	cin >> a;
+
+	for (int i = 1; i <= a; i++)
+	{
+		cin >> b;
+		s.push(b);
+	}
+	for (int j = 1; j <= a; j++)
+	{
+		cout << s.top();
+		s.pop();
+	}
+}
