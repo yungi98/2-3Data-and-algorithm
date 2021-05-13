2845bbf8406063c472942ebbde00d12d59701a4f
최윤기 <dbsrldufao12@gmail.com>
2021-05-13 18:50:53 +0900

    자료구조와 게임 알고리즘 스택실습3
    천 단위 구분 기호(스택 사용)

---
+++ Data/c++_24/c++_24/c++_24.cpp
@@ -0,0 +1,36 @@
+﻿//20318 최윤기
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+stack<char> num;
+stack<char> num2;
+
+int main()
+{
+	cout << "수를 입력해 주세요." << endl;
+	string su;
+	getline(cin, su);
+
+	for (int i = 0; i <= su.size() - 1; i++)
+	{
+		num.push(su[i]);
+	}
+	for (int j = 1; !num.empty(); j++)
+	{
+		if (j == 4)
+		{
+			num2.push(',');
+			j = 1;
+		}
+		num2.push(num.top());
+		num.pop();
+	}
+
+	for (; !num2.empty(); )
+	{
+		cout << num2.top();
+		num2.pop();
+	}
+}
